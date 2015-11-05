ff8217eff65156f047e0b7f7b5fafac14da419ba
Alan Honeycutt <ahoneycutt@massmutual.com>
2015-11-05 11:10:24 -0500

    Using carthage generated dynamic framework (resulting in link error)

---
+++ HelloClient/HelloClient/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  HelloClient
+//
+//  Created by AHoneycutt on 11/5/15.
+//  Copyright © 2015 AHoneycutt. All rights reserved.
+//
+
+import UIKit
+import SimpleDynamicFramework
+
+class ViewController: UIViewController {
+}
